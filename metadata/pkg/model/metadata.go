6a0deb201f8c69769b391ccc4bc1697319f77493
Yuvraj Neema <yuvraj.neema.cd.che19@itbhu.ac.in>
2025-01-25 22:47:29 +0530

    Few Lint Fixes

--- metadata/pkg/model/metadata.go
+++ metadata/pkg/model/metadata.go
@@ -3,8 +3,8 @@ package model
 // Metadata defines the movie metadata.
 
 type Metadata struct {
-	ID          string `json:"id`
-	Title       string `json:"title`
-	Description string `json:"description`
-	Director    string `json:"director`
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Director    string `json:"director"`
 }
--- movie/pkg/model/model.go
+++ movie/pkg/model/model.go
@@ -2,8 +2,8 @@ package model
 
 import "movieexample.com/metadata/pkg/model"
 
-// Movie Details includes movie metadata and its aggregate rating
+// MovieDetails includes movie metadata its aggregated rating.
 type MovieDetails struct {
-	Rating   *float64       `json:"rating,omitEmpty"`
-	Metadata model.Metadata `json:"metadata`
+	Rating   *float64       `json:"rating,omitempty"`
+	Metadata model.Metadata `json:"metadata"`
 }
